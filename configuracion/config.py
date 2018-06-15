eb07ea1f9e573062b949438037214d6189750964
sergiolara97 <37382569+sergiolara97@users.noreply.github.com>
2018-06-15 22:33:06 +0200

    Add files via upload

---
+++ configuracion/config.py
@@ -0,0 +1,9 @@
+confi_user='root'
+confi_pass='inves'
+confi_host='localhost'
+confi_database='programa'
+ 
+config_mysql = {'user': confi_user,
+            'password': confi_pass,
+            'host': confi_host,
+            'database': confi_database, }
